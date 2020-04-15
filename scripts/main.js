518e3f927e2a2172451c3573dd16b4028b0af01a
BuildTools <unconfigured@null.spigotmc.org>
2020-04-15 16:07:50 -0600

    got everything (d3, observable cells) integrated and working in browser

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+async function loadData() {
+    const metadata = await d3.json("data/esmond_examples/metadata.json");
+    const packets = await d3.json("data/esmond_examples/packets.json");
+    return ({metadata: metadata, traces: packets});
+}
+
+loadData().then((result) => {
+    console.log(inferNetworkGraph(result.traces));
+})
