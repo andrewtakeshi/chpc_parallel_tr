5c3ceae2fc3a582f2cb70c2abfcf7cc93cb9189e
Andrew Golightly <u0979377@umail.utah.edu>
2020-07-14 15:17:46 -0600

    Added number of runs.

--- scripts/home.js
+++ scripts/home.js
@@ -57,6 +57,8 @@ function runTable()
     if (document.getElementById("esmond_ip_dest").value === "")
         return;
 
+    document.getElementById("current_run_table_area").style.visibility = "visible";
+
     let row = document.getElementById("cr_table").insertRow();
     for (let i = 0; i < 6; i++)
     {
@@ -66,12 +68,13 @@ function runTable()
     let source = document.getElementById("esmond_ip_source").value;
     let dest = document.getElementById("esmond_ip_dest").value;
     let type = source ? "pScheduler" : "System";
+    let numRuns = document.getElementById("esmond_num_runs").value;
 
 
     row.cells[0].innerHTML = type;
     row.cells[1].innerHTML = source ? source : "None";
     row.cells[2].innerHTML = dest;
-    row.cells[3].innerHTML = "1";
+    row.cells[3].innerHTML = numRuns;
     row.cells[4].innerHTML = "Running"
     row.cells[5].style.textAlign = "center";
     row.cells[5].innerHTML = "<input type=\"checkbox\" checked=\"checked\">"
